5506fb1d301109d2c5bcd8936908e72a0cd63239
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-15 00:39:05 +0700

    Hackety hack (unfinished)

---
+++ lib/App/manwrap/pm.pm
@@ -0,0 +1,16 @@
+package App::manwrap::pm;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: man + Perl stuffs
+
+=head1 SYNOPSIS
+
+See L<manwrap-pm> included in this distribution.
+
+
+=head1 SEE ALSO
+
+L<pmman> from L<App::PMUtils>
